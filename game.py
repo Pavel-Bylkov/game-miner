a450ebc3d61697df66a029481337a895804df50c
Pavel-Bylkov <oldman.bpv@gmail.com>
2021-05-22 14:21:38 +0300

    add img hero

--- game.py
+++ game.py
@@ -11,7 +11,8 @@ background = pg.transform.scale(pg.image.load(IMG_FON), (WIN_X, WIN_Y))
 
 walls = pg.sprite.Group()
 walls.add(
-    Wall(img=IMG_WALL, x=0, y=WIN_Y-50, size_x=WIN_X+30, size_y=50)
+    Wall(img=IMG_WALL, x=0, y=WIN_Y-50, size_x=WIN_X+30, size_y=50),
+    Wall(img=IMG_WALL, x=WIN_X//2, y=WIN_Y//2, size_x=WIN_X//2, size_y=50)
 )
 
 clock = pg.time.Clock()
--- tools/constants.py
+++ tools/constants.py
@@ -9,3 +9,5 @@ IMG_FON = f"img{os.sep}brick-wall.jpg"
 IMG_WALL = f"img{os.sep}plat.png"
 IMG_WALL_L = f"img{os.sep}plat_l.png"
 IMG_WALL_R = f"img{os.sep}plat_r.png"
+
+IMGS_HERO = [f"img{os.sep}Asset 59@4x.png", f"img{os.sep}Asset 73@4x.png", f"img{os.sep}Asset 60@4x.png"]
